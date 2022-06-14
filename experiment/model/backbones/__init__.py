b5e328b7cea4bd4f2c3603c307a8f5acef7149e8
Terence Wang <wangteng18@mails.ucas.ac.cn>
2022-06-14 19:23:59 +0800

    Add configuration for mobilenet v2

--- experiment/model/backbones/__init__.py
+++ experiment/model/backbones/__init__.py
@@ -2,3 +2,4 @@ from experiment.model.backbones.factory import build_backbone
 from experiment.model.backbones.factory import register_backbone_builder
 
 from experiment.model.backbones.resnet import ResNet
+from experiment.model.backbones.mobilenet import MobileNet
