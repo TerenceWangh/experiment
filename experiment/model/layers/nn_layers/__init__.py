e0821ceafc430975969d85f8da4e6396f44b29cc
Terence Wang <wangteng18@mails.ucas.ac.cn>
2022-06-28 16:30:11 +0800

    Support transformer in classification

--- experiment/model/layers/nn_layers/__init__.py
+++ experiment/model/layers/nn_layers/__init__.py
@@ -1,3 +1,6 @@
 from experiment.model.layers.nn_layers.squeeze_excitation import SqueezeExcitation
 from experiment.model.layers.nn_layers.stochastic_depth import StochasticDepth
 from experiment.model.layers.nn_layers.stochastic_depth import get_stochastic_depth_rate
+from experiment.model.layers.nn_layers.transformer import AddPositionEmbed
+from experiment.model.layers.nn_layers.transformer import VisionTransformerToken
+from experiment.model.layers.nn_layers.transformer import VisionTransformerEncoder
